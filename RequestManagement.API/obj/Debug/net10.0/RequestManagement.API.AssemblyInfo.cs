5f6c26c4b55557351eae462c9c1e44d70276286d
Emin Samadzade <eminsemedzade7@gmail.com>
2026-04-22 10:08:37 +0400

    Add launch configuration and tasks

--- RequestManagement.API/obj/Debug/net10.0/RequestManagement.API.AssemblyInfo.cs
+++ RequestManagement.API/obj/Debug/net10.0/RequestManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RequestManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd617c548e66315f4b5487888ca08d4f79979b05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1857ef3963747dd9d1683662aa6decfb54cb51")]
 [assembly: System.Reflection.AssemblyProductAttribute("RequestManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RequestManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
